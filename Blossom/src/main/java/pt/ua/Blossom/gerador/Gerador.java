a7daf282a4c6b1ecad2350fcc3e55f78ec4040b0
unknown <lvalentim@ua.pt>
2020-12-10 09:33:01 +0000

    gerador de dados implementação inicial

---
+++ Blossom/src/main/java/pt/ua/Blossom/gerador/Gerador.java
@@ -0,0 +1,83 @@
+package pt.ua.Blossom.gerador;
+
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+public class Gerador {
+	
+	static int num_of_ph_monitors = 1;  		// Numero de monitores de ph que iremos gerar.
+	static int num_of_hum_monitors = 1; 		// Numero de monitores de humidade que iremos gerar.
+	static int freq = 48; 					// Numero de registos que os monitores fazem por dia.
+	static int days = 1;
+	static String monitor_type[] = {"Low", "Medium", "High"};     // Gerar monitores especificos
+																		   // Low -> ph acido / humidade baixa
+																		   // Medium -> ph neutro / humidade média
+																		   // High -> ph básico / humidade alta
+
+	public static void main(String[] args) throws InterruptedException{
+		
+		ArrayList<MonitorPH> monitoresPH = new ArrayList<MonitorPH>();
+		ArrayList<MonitorHum> monitoresHum = new ArrayList<MonitorHum>();
+		
+		//Ciclo para gerar monitores de PH
+		for(int i = 0; i<num_of_ph_monitors; i++) {
+			int opt = getRandomNumberUsingInts(0, 2);
+			MonitorPH ph = new MonitorPH(monitor_type[opt], freq, days);
+			monitoresPH.add(ph);
+		}
+		
+		//Ciclo para gerar monitores de humidade
+		for(int i = 0; i<num_of_hum_monitors; i++) {
+			int opt = getRandomNumberUsingInts(0, 2);
+			MonitorHum humid = new MonitorHum(monitor_type[opt], freq, days);
+			monitoresHum.add(humid);
+		}
+		
+		if(days == 0) { //Não gerar valores automaticamente, gerar em tempo real
+			while(true) {
+				
+				//Adicionar 1 medição aos monitores de PH
+				for(int i = 0; i<monitoresPH.size(); i++) {
+					MonitorPH p = monitoresPH.get(i);
+					String mode = p.getMode();
+					if(mode == "Low") {
+						p.generateAcidicSoil(1/freq);
+					}
+					if(mode == "Medium") {
+						p.generateNeutralSoil(1/freq);
+					}
+					if(mode == "High") {
+						p.generateBasicSoil(1/freq);
+					}
+				}
+				
+				//Adicionar 1 medição aos monitores de humidade
+				for(int i = 0; i<monitoresHum.size(); i++) {
+					MonitorHum h = monitoresHum.get(i);
+					String mode = h.getMode();
+					if(mode == "Low") {
+						h.generateDrySoil(1/freq);
+					}
+					if(mode == "Medium") {
+						h.generateNeutralSoil(1/freq);
+					}
+					if(mode == "High") {
+						h.generateMoistSoil(1/freq);
+					}
+				}
+				
+				//Esperar tempo real pela proxima medição
+				int real_freq = (1/(freq/24));
+				TimeUnit.MINUTES.sleep(real_freq * 60);
+			}
+			
+		}
+	}
+	
+	public static int getRandomNumberUsingInts(int min, int max) {  //Retirado de https://www.baeldung.com/java-generating-random-numbers-in-range
+	    Random random = new Random();
+	    return random.ints(min, max)
+	      .findFirst()
+	      .getAsInt();
+	}
+}
---
+++ Blossom/src/main/java/pt/ua/Blossom/gerador/MonitorPH.java
@@ -0,0 +1,75 @@
+package pt.ua.Blossom.gerador;
+
+import java.util.*;
+
+public class MonitorPH {
+		
+		private int id;
+		private int ownerId;
+		private String modo;
+		private int reg_freq;
+		private ArrayList<Integer> values;	
+
+		public MonitorPH(String mode, int freq, int instant) {		//instant é uma opção que permite gerar automaticamente valores de um certo numero de dias
+			this.modo = mode;
+			this.reg_freq = freq;
+			this.id = 0;
+			if(instant > 0) {
+				if(this.modo == "Low") {
+					this.generateAcidicSoil(instant);
+				}
+				if(this.modo == "Medium") {
+					this.generateNeutralSoil(instant);
+				}
+				if(this.modo == "High") {
+					this.generateBasicSoil(instant);
+				}
+			}
+		}
+		
+		public void generateAcidicSoil(int days) {
+			if(days>0) {
+				for(int i = 0; i<this.reg_freq; i++) {
+					int value = getRandomNumberUsingInts(0,7);
+					this.values.add(value);
+				}
+			}
+		}
+		
+		public void generateNeutralSoil(int days) {
+			if(days>0) {
+				for(int i = 0; i<this.reg_freq; i++) {
+					int value = getRandomNumberUsingInts(4,10);
+					this.values.add(value);
+				}
+			}
+		}
+		
+		public void generateBasicSoil(int days) {
+			if(days>0) {
+				for(int i = 0; i<this.reg_freq; i++) {
+					int value = getRandomNumberUsingInts(7,14);
+					this.values.add(value);
+				}
+			}
+		}
+		
+		public ArrayList<Integer> getValues(){
+			return this.values;
+		}
+		
+		public int getId() {
+			return this.id;
+		}
+		
+		public String getMode() {
+			return this.modo;
+		}
+		
+		public int getRandomNumberUsingInts(int min, int max) {  //Retirado de https://www.baeldung.com/java-generating-random-numbers-in-range
+		    Random random = new Random();
+		    return random.ints(min, max)
+		      .findFirst()
+		      .getAsInt();
+		}
+	}
