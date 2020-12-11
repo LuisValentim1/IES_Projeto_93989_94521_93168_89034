97cd30a082d55cdb49472cfa549816d0ca524aef
renanaferreira <renanaferreira1999@gmail.com>
2020-12-11 15:01:15 +0000

    resolvido profile page, return user and a list of parcels

--- blossom/src/main/java/com/ies/blossom/repositorys/ParcelRepository.java
+++ blossom/src/main/java/com/ies/blossom/repositorys/ParcelRepository.java
@@ -1,9 +1,17 @@
 package com.ies.blossom.repositorys;
 
 import com.ies.blossom.entitys.Parcel;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ParcelRepository extends JpaRepository<Parcel, Long> {
+
+    //@Query("select p from parcels p where p.owner_id = ?1")	
+    //List<Parcel> findByUser(long user_id);
+	
 }
