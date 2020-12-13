2418ff763c8f01c2fb4c92b74da50381964da526
Orlando-pt <orlando.macedo15@ua.pt>
2020-12-13 18:56:52 +0000

    adicionar a bd à raiz da app; fazer a ligacao dos dados da db até ao frontend para as seguintes páginas : humsensor.html, measures.html, parcel.html

--- blossom/src/main/java/com/ies/blossom/controllers/HumSensorController.java
+++ blossom/src/main/java/com/ies/blossom/controllers/HumSensorController.java
@@ -1,12 +1,17 @@
 package com.ies.blossom.controllers;
 
 import java.sql.Date;
+import java.util.List;
 
 import com.ies.blossom.entitys.Parcel;
+import com.ies.blossom.entitys.HumMeasure;
 import com.ies.blossom.entitys.HumSensor;
 import com.ies.blossom.entitys.User;
+import com.ies.blossom.model.SensorModel;
+import com.ies.blossom.repositorys.HumMeasureRepository;
 import com.ies.blossom.repositorys.HumSensorRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,11 +22,17 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @Controller
 public class HumSensorController {
 
+    @Autowired
+    private HumSensorRepository humSensorRepository;
+
+    @Autowired
+    private HumMeasureRepository humMeasureRepository;
+    /* comento pq n sei qual era a intencao ao colocar o repo no construtor
     HumSensorRepository humSensorRepository;
 
     public HumSensorController(HumSensorRepository humSensorRepository) {
         this.humSensorRepository = humSensorRepository;
-    }
+    }*/
 
     // TODO Change the URL MAPPING TO parcel NAME DYNAMICALY ex. phsensor.id //
 
@@ -29,9 +40,24 @@ public class HumSensorController {
     public String getIssues(Model model) {
 
         // Temporary Values
-        Parcel tempParcel = new Parcel();
-        java.sql.Date tempDate = new java.sql.Date(0);
-        model.addAttribute("humSensor", new HumSensor(tempParcel, tempDate));
+//        Parcel tempParcel = new Parcel();
+//        java.sql.Date tempDate = new java.sql.Date(0);
+//        model.addAttribute("humSensor", new HumSensor(tempParcel, tempDate));
+        List<HumSensor> sensors = this.humSensorRepository.findAll(); // Normalmente quando se chegasse a esta página já se
+                                                            // saberia qual era o id do sensor em causa
+                                                            // desta forma vamos buscar o 1o sensor da tabela TODO ir buscar 1
+                                                            // q faca sentido
+        HumSensor sensor = sensors.get(0);
+        model.addAttribute("humSensor", sensor);
+
+        model.addAttribute("last_med", sensor.getMeasures().get(0));
         return "humSensor.html";
+    }
+    // humSensor/measures?sensorId=5
+    @GetMapping("/humSensor/measures")
+    public String showMeasures(Model model, @RequestParam(name="sensorId", required = true) Long sensorId) {
+        List<HumMeasure> measures = this.humMeasureRepository.findBySensorId(sensorId);
+        model.addAttribute("measures", measures);
+        return "measures.html";
     }
 }
