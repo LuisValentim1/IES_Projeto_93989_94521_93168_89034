cea96178f077f428f07975731d3b05297f3dea1e
Orlando-pt <orlando.macedo15@ua.pt>
2020-12-13 20:09:07 +0000

    dados para plant.html

--- blossom/src/main/java/com/ies/blossom/controllers/PlantController.java
+++ blossom/src/main/java/com/ies/blossom/controllers/PlantController.java
@@ -3,9 +3,11 @@ package com.ies.blossom.controllers;
 import com.ies.blossom.entitys.Plant;
 import com.ies.blossom.repositorys.PlantRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -13,17 +15,20 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @Controller
 public class PlantController {
 
+    @Autowired
+    private PlantRepository plantRepository;
+    /*
     PlantRepository plantRepository;
 
     public PlantController(PlantRepository plantRepository) {
         this.plantRepository = plantRepository;
-    }
+    }*/
 
     //TODO Change the URL MAPPING TO PLANT NAME DYNAMICALY //
 
-    @GetMapping("/plant")
-    public String getIssues(Model model) {
-        model.addAttribute("plant", new Plant("RobertoCience", "Roberto", 2.0, 3.0, 4.0, 5.1));
+    @GetMapping("/plant/{id}")
+    public String getIssues(Model model, @PathVariable(value = "id") Long plantId) {
+        model.addAttribute("plant", this.plantRepository.getOne(plantId));
         return "plant.html";
     }
 }
