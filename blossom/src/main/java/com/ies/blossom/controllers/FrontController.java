a4df19b6cd958d847dc16ae3c23ecb2b6f5d54d0
Orlando-pt <orlando.macedo15@ua.pt>
2021-01-02 19:30:23 +0000

    charts para admin

--- blossom/src/main/java/com/ies/blossom/controllers/FrontController.java
+++ blossom/src/main/java/com/ies/blossom/controllers/FrontController.java
@@ -9,7 +9,6 @@ import com.ies.blossom.repositorys.UserRepository;
 import com.ies.blossom.security.CustomUserDetails;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
@@ -17,7 +16,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 @Controller
 public class FrontController {
@@ -48,8 +50,62 @@ public class FrontController {
         List<User> users = this.userRepository.findAllUsersNotAdmin();
 
         model.addAttribute("users", users);
+        
+        model.addAttribute("joinedData", this.getJoinedData(users));
+        model.addAttribute("lastJoinedData", this.getLastJoinedData(users));
 
         return "admin.html";
+    }
+
+    private String getLastJoinedData(List<User> users) {
+        String ret = "";
+        Map<String, Integer> map = new TreeMap<String, Integer>();
+
+        for (User user : users) {
+            String date = new Date(user.getLastJoined().getTime()).toString();
+            if (!map.containsKey(date)) {
+                map.put(date, 1);
+            } else {
+                map.put(date, map.get(date) + 1);                
+            }
+        }
+
+        Iterator<Map.Entry<String, Integer>> it = map.entrySet().iterator();
+
+        while(it.hasNext()) {
+            Map.Entry<String, Integer> entry = it.next();
+            ret += entry.getKey() + " " + entry.getValue().toString();
+
+            if (it.hasNext())
+                ret += "DELIMITER";
+        }
+
+        return ret;
+    }
+
+    private String getJoinedData(List<User> users) {
+        String ret = "";
+        Map<String, Integer> map = new TreeMap<String, Integer>();
+
+        for (User user : users) {
+            if (!map.containsKey(user.getEntryDate().toString())) {
+                map.put(user.getEntryDate().toString(), 1);
+            } else {
+                map.put(user.getEntryDate().toString(), map.get(user.getEntryDate().toString()) + 1);                
+            }
+        }
+
+        Iterator<Map.Entry<String, Integer>> it = map.entrySet().iterator();
+
+        while(it.hasNext()) {
+            Map.Entry<String, Integer> entry = it.next();
+            ret += entry.getKey() + " " + entry.getValue().toString();
+
+            if (it.hasNext())
+                ret += "DELIMITER";
+        }
+
+        return ret;
     }
 
     @GetMapping("/contact")
